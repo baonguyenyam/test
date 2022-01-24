b65e7fe772f3d08c4d986bcd68f9485a75066707
Nguyen Pham <baonguyenyam@gmail.com>
2022-01-24 10:58:08 -0600

    OK

---
+++ dist/js/main.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// Main
+// FIXED SVG SPACING AROUND 
+$(document).ready(function () {
+  $.each($('svg, symbol'), function () {
+    $(this).attr('x', '0');
+    $(this).attr('y', '0');
+  });
+  $('#icon-shape-1').attr('preserveAspectRatio', 'xMaxYMax slice');
+  $('#icon-shape-2').attr('preserveAspectRatio', 'xMaxYMax meet');
+});
+//# sourceMappingURL=main.js.map
